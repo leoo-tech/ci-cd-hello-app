be4c86eef3e5a95688b9f4eae7b0912dc52d374e
Leonardo Costa <leoonematrix@gmail.com>
2025-07-21 19:50:01 -0300

    Alterar mensagem de retorno da rota raiz para uma citação sobre automação

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "A automação é uma miragem. Ela parece distante, mas com paciência e dedicação, você pode alcançá-la."}
